4f66a301e614ae8ee88262fcad52b42f0f6facff
Matt Heffernan <slithymatt@hotmail.com>
2021-05-20 01:41:16 -0400

    tile offset mostly working properly

--- pop.asm
+++ pop.asm
@@ -1,9 +1,9 @@
-DEVICE ZXSPECTRUM48
+   DEVICE ZXSPECTRUM48
 
-org $8000
+   org $8000
 
 start:
-jp init
+   jp init
 
 ; Memory Segments
 SCREEN_MEM  = $4000
@@ -13,17 +13,21 @@ COLOR_ATTR  = $5800
 ENTER       = $0D
 
 tiles:
-include tiles.asm
+   include tiles.asm
 
 tile_map:
    block (32 * 24),0
 
-include bubbles.asm
-include video.asm
+   include bubbles.asm
+   include video.asm
 
 init:
    call fill_rows
+dbg:
+   nop
+   jr dbg
    call render_tiles
+   jp init ; TODO not just be an infinite loop!
    ret
 
 
@@ -31,8 +35,8 @@ init:
 LENGTH      = $ - start
 
 ; option 1: tape
-include TapLib.asm
-MakeTape ZXSPECTRUM48, "pop.tap", "pop", start, LENGTH, start
+   include TapLib.asm
+   MakeTape ZXSPECTRUM48, "pop.tap", "pop", start, LENGTH, start
 
 ; option 2: snapshot
-SAVESNA "pop.sna", start
+   SAVESNA "pop.sna", start
