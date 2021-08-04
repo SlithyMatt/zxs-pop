bd1de9baa1658a08046b3ee69e78b6b0aca549d6
Matt Heffernan <slithymatt@hotmail.com>
2021-08-03 23:04:23 -0400

    incomplete path toward refresh rectangle

--- video.asm
+++ video.asm
@@ -1,6 +1,9 @@
 refresh_rectangle:
    db 0,0,32,24            ; x_start, y_start, x_end, y_end
 
+rectangle_width:
+   db 0
+
 set_rectangle: ; BC = start x,y; DE = end x,y
    ld a,(refresh_rectangle)
    cp b
@@ -14,12 +17,60 @@ set_rectangle: ; BC = start x,y; DE = end x,y
    ld a,c
    ld (refresh_rectangle+1),a
 @check_d:
+   ld a,(refresh_rectangle+2)
+   cp d
+   jr nc,@check_e
+   ld a,d
+   ld (refresh_rectangle+2),a
+@check_e:
+   ld a,(refresh_rectangle+3)
+   cp e
+   jr nc,@done_set_rec
+   ld a,e
+   ld (refresh_rectangle+3),a
+@done_set_rec:
    ret
 
 render_tiles:
    ld ix,tile_map          ; ix = tile index
-   ld bc,DBL_BUFFER        ; bc = start of tile
-   ld de,32*24             ; de = loop index
+   ld d,0
+   ld a,(refresh_rectangle+1)
+   ld e,a
+   sla e
+   rl d
+   sla e
+   rl d
+   sla e
+   rl d
+   sla e
+   rl d
+   sla e
+   rl d
+   ld a,(refresh_rectangle)
+   or e
+   ld e,a
+   add ix,de
+   ld hl,DBL_BUFFER        ; bc = start of tile
+   sla d
+   sla d
+   sla d
+   ld a,$40
+   or d
+   ld d,a
+   add hl,de
+   push hl
+   pop bc
+   ld a,(refresh_rectangle)
+   ld d,a
+   ld a,(refresh_rectangle+2)
+   sub d
+   ld (rectangle_width),a
+   ld d,a                  ; d = rectangle width
+   ld a,(refresh_rectangle+1)
+   ld e,a
+   ld a,(refresh_rectangle+3)
+   sub e
+   ld e,a                  ; e = rectangle height
 @tile_loop:
    push bc                 ; stash bc on stack
    ld b,0
@@ -41,6 +92,11 @@ render_tiles:
    ld a,$07
    and b
    jr nz,@write_loop
+   ld a,(rectangle_width)
+   cp 32
+   jr z,@full_width
+   
+@full_width:
    ld a,b
    sub 8
    ld b,a                  ; bc = start of tile, again
@@ -56,6 +112,7 @@ render_tiles:
    or d
    or e
    jp nz,@tile_loop
+@end_render:
    ret
 
 copy_dbl_buffer:
